e5723e74e4a7e51933946d4dd37672c937fb75db
kimsukdong <sdk1005@naver.com>
2021-11-17 11:14:13 +0900

    update at 11-17

--- javafx2/src/application/Main.java
+++ javafx2/src/application/Main.java
@@ -3,6 +3,7 @@ package application;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
@@ -14,6 +15,8 @@ public class Main extends Application {
 			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
 			Scene scene = new Scene(root,300,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			Image img = new Image("C:\\Users\\SDK\\git\\javafx_edu2\\javafx2\\src\\application\\cal.png");
+			primaryStage.getIcons().add(img);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 			primaryStage.setTitle("계산기 예제");
